0bce9ca40a3c0ea071b76e882457e6a58595cf07
Leonardo Fin <leonardo.fin@resultadosdigitais.com.br>
2017-09-16 16:35:24 -0300

    finished book crud

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.fetch(:book, {}).permit(:title, :description)
+      params.fetch(:book, {}).permit(:title, :description, :author, :url)
     end
 end
