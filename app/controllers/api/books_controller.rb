e4bc70969c00efaa57cca71154491a98c951974a
Leonardo Fin <leonardo.fin@resultadosdigitais.com.br>
2017-09-17 00:40:55 -0300

    fixed postgres configuration

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -7,13 +7,15 @@ module Api
       query = filters[:query]
       author_filter = filters[:author]
 
-      books = Book.all
+      books = Book
 
       if query
         query = "%" + query + "%"
         books = Book.where("title ilike ? or description ilike ?", query, query) 
       end
       books = books.where("author ilike ?", "%" + author_filter + "%") if author_filter
+
+      books = books.all unless query || author_filter
 
       render json: {data: books, total: books.count}
     end
